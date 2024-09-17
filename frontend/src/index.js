9086939bb22e0cc82bd5bbf9e0f2a6fee88c1d1c
bt-dung <94605325+bt-dung@users.noreply.github.com>
2024-09-17 15:40:50 +0700

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from '~/components/GlobalStyles';
+const apiUrl = process.env.REACT_APP_API_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
